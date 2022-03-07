d0fb461c92d358ff3dfb5de9a879c91eec58b999
Elton Bautista <eltonbestaha@gmail.com>
2022-03-07 10:10:59 -0700

    Just finished doing webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: path.resolve(__dirname, 'src/index.js')
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        open: true,
+        hot: true,
+      },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+        clean: true,
+        publicPath: '/',
+
+    },
+    module: {
+        rules: [
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: 'asset/resource',
+          },
+          {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: 'asset/resource',
+          },
+        ],
+      },
+      plugins: [
+          new HtmlWebpackPlugin({
+              title: 'Todo List',
+              filename: 'index.html',
+              template: path.resolve(__dirname, 'src/template.html'),
+          }),
+      ],
+}
